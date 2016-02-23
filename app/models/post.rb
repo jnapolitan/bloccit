a9491b6ee46a32521578298e8b4936ea4499c477
Julian Napolitan <julian.napolitan@gmail.com>
2016-02-22 21:19:25 -0800

    Assignment 39 completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,9 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments, dependent: :destroy
 
-  default_scope { oder('created_at DESC') }
+  default_scope { order('created_at DESC') }
+  scope :ordered_by_title, -> { reorder(title: :asc) }
+  scope :ordered_by_reverse_created_at, -> { reorder(created_at: :asc) }
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
