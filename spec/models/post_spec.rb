10419b83ad7fdb1c4df04e7017293011fff179d8
Julian Napolitan <jnapolitan@twitter.com>
2016-02-29 21:38:29 -0800

    Checkpoint 41 completed

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -9,6 +9,8 @@ RSpec.describe Post, type: :model do
 
   let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user) }
 
+  it { is_expected.to have_many(:comments) }
+
   it { is_expected.to belong_to(:topic) }
   it { is_expected.to belong_to(:user) }
 
