1fed25dd082e9b94fcb2611c76a9cf02e7e2482e
Julian Napolitan <jnapolitan@twitter.com>
2016-03-05 17:06:33 -0800

    Assignment 42 help

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,8 +1,9 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
+  has_many :comments, as: :commentable, dependent: :destroy
 
   has_many :labelings, as: :labelable
 
   has_many :labels, through: :labelings
-  
+
 end
